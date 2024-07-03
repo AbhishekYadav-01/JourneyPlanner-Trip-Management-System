e140e8b3ac34ae9e5b8e9ab01d1a83f6d6c81c5f
Abhishek Yadav <145996514+AbhishekYadav-01@users.noreply.github.com>
2024-07-03 11:25:02 +0530

    Create Trip_Class.cpp
    This is Trip class code.

---
+++ Code/Trip.cpp
@@ -0,0 +1,54 @@
+class Trip {
+public:
+    Trip(Vehicle* vehicle, std::string pick_up_location, std::string drop_location, int departure_time)
+        : vehicle(vehicle), pick_up_location(pick_up_location), drop_location(drop_location), departure_time(departure_time), booked_seats(0) {}
+
+    // Getter functions
+    Vehicle* getVehicle() const {
+        return vehicle;
+    }
+
+    std::string getPickUpLocation() const {
+        return pick_up_location;
+    }
+
+    std::string getDropLocation() const {
+        return drop_location;
+    }
+
+    int getDepartureTime() const {
+        return departure_time;
+    }
+
+    int getBookedSeats() const {
+        return booked_seats;
+    }
+
+    // Setter functions
+    void setVehicle(Vehicle* v) {
+        vehicle = v;
+    }
+
+    void setPickUpLocation(std::string location) {
+        pick_up_location = location;
+    }
+
+    void setDropLocation(std::string location) {
+        drop_location = location;
+    }
+
+    void setDepartureTime(int time) {
+        departure_time = time;
+    }
+
+    void setBookedSeats(int seats) {
+        booked_seats = seats;
+    }
+
+private:
+    Vehicle* vehicle;
+    std::string pick_up_location;
+    std::string drop_location;
+    int departure_time;
+    int booked_seats;
+};
