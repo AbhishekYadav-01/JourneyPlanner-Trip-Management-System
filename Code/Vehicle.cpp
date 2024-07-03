36ccbbf18b1f9936683fe7288240249411be1b76
Abhishek Yadav <145996514+AbhishekYadav-01@users.noreply.github.com>
2024-07-03 11:27:38 +0530

    Create Vehicle.cpp

---
+++ Code/Vehicle.cpp
@@ -0,0 +1,34 @@
+class Vehicle {
+public:
+    Vehicle(std::string vehicle_number, int seating_capacity)
+        : vehicle_number(vehicle_number), seating_capacity(seating_capacity) {}
+
+    // Getter functions
+    std::string getVehicleNumber() const {
+        return vehicle_number;
+    }
+
+    int getSeatingCapacity() const {
+        return seating_capacity;
+    }
+
+    // Setter functions
+    void setVehicleNumber(std::string number) {
+        vehicle_number = number;
+    }
+
+    void setSeatingCapacity(int capacity) {
+        seating_capacity = capacity;
+    }
+
+    void addTrip(Trip* trip) {
+        trips.push_back(trip);
+    }
+    vector<Trip*> getTrips(){
+        return trips;
+    }
+private:
+    std::string vehicle_number;
+    int seating_capacity;
+    std::vector<Trip*> trips;
+};
