976f19b04f7487bce520a462524ca963ffd232c5
Abhishek Yadav <145996514+AbhishekYadav-01@users.noreply.github.com>
2024-07-03 11:26:19 +0530

    Create BinaryTreeNode.cpp

---
+++ Code/BinaryTreeNode.cpp
@@ -0,0 +1,54 @@
+class BinaryTreeNode {
+public:
+    BinaryTreeNode(int departuretime = 0, Trip* tripenodeptr = nullptr, BinaryTreeNode* parentptr = nullptr)
+        : leftptr(nullptr), rightptr(nullptr), parentptr(parentptr), departuretime(departuretime), tripnodeptr(tripenodeptr) {}
+
+    // Getter functions
+    BinaryTreeNode* getLeftPtr() const {
+        return leftptr;
+    }
+
+    BinaryTreeNode* getRightPtr() const {
+        return rightptr;
+    }
+
+    BinaryTreeNode* getParentPtr() const {
+        return parentptr;
+    }
+
+    int getDepartureTime() const {
+        return departuretime;
+    }
+
+    Trip* getTripNodePtr() const {
+        return tripnodeptr;
+    }
+
+    // Setter functions
+    void setLeftPtr(BinaryTreeNode* left) {
+        leftptr = left;
+    }
+
+    void setRightPtr(BinaryTreeNode* right) {
+        rightptr = right;
+    }
+
+    void setParentPtr(BinaryTreeNode* parent) {
+        parentptr = parent;
+    }
+
+    void setDepartureTime(int time) {
+        departuretime = time;
+    }
+
+    void setTripNodePtr(Trip* trip) {
+        tripnodeptr = trip;
+    }
+
+private:
+    BinaryTreeNode* leftptr;
+    BinaryTreeNode* rightptr;
+    BinaryTreeNode* parentptr;
+    int departuretime;
+    Trip* tripnodeptr;
+};
